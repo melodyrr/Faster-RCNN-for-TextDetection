c1bf11be5dbdc71c071b230ae18d5537ad8eeefa
melodyrr <melody_77777@163.com>
2020-07-21 00:09:00 +0800

    first commit

---
+++ data/io/make_tfrecord.sh
@@ -0,0 +1,1 @@
+python convert_data_to_tfrecord.py --VOC_dir /home/melody/media/study_space/data/icdar2015/train/ --xml_dir hbbxml --image_dir images --save_name train --img_format .jpg --dataset icdar
---
+++ tools/inference.sh
@@ -0,0 +1,1 @@
+python inference.py --data_dir /home/melody/media/study_space/data/icdar2015/test/images/ --save_dir RESULTS1 --GPU 0
